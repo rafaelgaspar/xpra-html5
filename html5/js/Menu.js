7568a49fae1295d1f04bfa8f90802d11ed2c98e0
Rafael Antunes <me@rafaelgaspar.xyz>
2026-09-14 10:49:43 +0200

    feat(html5): fix submenu show/hide and outside-click handling

--- html5/js/Menu.js
+++ html5/js/Menu.js
@@ -45,14 +45,18 @@ SOFTWARE.
   function showMenu() {
     const menu = this;
     const ul = $("ul", menu)[0];
-    //hack to hide the menu from javascript
     if (!ul) {
       return;
     }
 
     if (ul.classList.contains(HIDE_CLASS_NAME)) {
       ul.classList.remove(HIDE_CLASS_NAME);
-      ul.parentElement.classList.remove(ACTIVE_CLASS_NAME);
+      menu.classList.add(ACTIVE_CLASS_NAME);
+      ul.classList.add(ANIMATING_CLASS_NAME);
+      ul.classList.add(VISIBLE_CLASS_NAME);
+      setTimeout(function() {
+        ul.classList.remove(ANIMATING_CLASS_NAME);
+      }, 25);
       return;
     }
 
@@ -79,65 +83,62 @@ SOFTWARE.
     setTimeout(function() {
       ul.classList.remove(VISIBLE_CLASS_NAME);
       ul.classList.remove(ANIMATING_CLASS_NAME);
-    }, 300);
+    }, 100);
   }
 
   function hideAllInactiveMenus() {
-    const menu = this;
-    forEach(
-      $(
-        `li.${HAS_SUBMENU_CLASS_NAME}.${ACTIVE_CLASS_NAME}:not(:hover)`,
-        menu.parent
-      ),
-      function(e) {
-        e.hideMenu && e.hideMenu();
+    forEach($(`li.${HAS_SUBMENU_CLASS_NAME}.${ACTIVE_CLASS_NAME}`, document), function(e) {
+      if (!e.matches(":hover")) {
+        const u = $("ul", e)[0];
+        if (!u || !u.matches(":hover")) {
+          e.hideMenu && e.hideMenu();
+        }
       }
-    );
+    });
   }
 
   function hideAllMenus() {
-    const menu = this;
-    forEach($(`li.${HAS_SUBMENU_CLASS_NAME}`, menu.parent), function(e) {
+    forEach($(`li.${HAS_SUBMENU_CLASS_NAME}`, document), function(e) {
       e.hideMenu && e.hideMenu();
     });
   }
 
-  window.addEventListener("load", function() {
-    forEach(
-      $(`.${MENU_CLASS_NAME} li.${HAS_SUBMENU_CLASS_NAME}`),
-      function(e) {
-        e.showMenu = showMenu;
-        e.hideMenu = hideMenu;
-      }
-    );
+  function onClickOutside(e) {
+    const fm = document.getElementById("float_menu");
+    if (fm && fm.contains(e.target)) {
+      return;
+    }
+    hideAllMenus();
+  }
 
-    forEach(
-      $(`.${MENU_CLASS_NAME} > li.${HAS_SUBMENU_CLASS_NAME}`),
-      function(e) {
-        e.addEventListener("click", showMenu);
-      }
-    );
+  function bindMenuHandlers() {
+    forEach($(`.${MENU_CLASS_NAME} li.${HAS_SUBMENU_CLASS_NAME}`), function(e) {
+      e.showMenu = showMenu;
+      e.hideMenu = hideMenu;
+    });
 
-    forEach(
-      $(`.${MENU_CLASS_NAME} > li.${HAS_SUBMENU_CLASS_NAME} li`),
-      function(e) {
-        e.addEventListener("mouseenter", hideAllInactiveMenus);
-      }
-    );
+    forEach($(`.${MENU_CLASS_NAME} > li.${HAS_SUBMENU_CLASS_NAME}`), function(e) {
+      e.addEventListener("click", showMenu);
+    });
 
-    forEach(
-      $(
-        `.${MENU_CLASS_NAME} > li.${HAS_SUBMENU_CLASS_NAME} li.${HAS_SUBMENU_CLASS_NAME}`
-      ),
-      function(e) {
-        e.addEventListener("mouseenter", showMenu);
-      }
-    );
+    forEach($("#menu_list>li,#startmenu>li,#startmenu li.-hasSubmenu,.Menu>li.-hasSubmenu li.-hasSubmenu"), function(e) {
+      e.addEventListener("mouseenter", hideAllInactiveMenus);
+    });
+
+    forEach($("#menu_list>li.-hasSubmenu,.Menu > li.-hasSubmenu li.-hasSubmenu"), function(e) {
+      e.addEventListener("mouseenter", showMenu);
+    });
 
     forEach($("a"), function(e) {
       e.addEventListener("click", hideAllMenus);
     });
 
-    document.addEventListener("click", hideAllInactiveMenus);
-  });
+    if (!window._xpraMenuOutsideBound) {
+      document.addEventListener("click", onClickOutside);
+      window._xpraMenuOutsideBound = 1;
+    }
+  }
+
+  window.rebindMenuHandlers = bindMenuHandlers;
+  window.addEventListener("load", bindMenuHandlers);
 })();
